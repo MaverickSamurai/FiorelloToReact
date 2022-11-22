8f3e44383b67bb8f28a6ab257f00c391eff48e8f
MaverickSamurai <mirsamirba@code.edu.az>
2022-11-23 01:37:35 +0400

    FiorelloToReact

---
+++ FiorelloToReact/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import FooterFR from './components/common/FooterFR';
+import HeaderFR from './components/common/HeaderFR';
+import Fiorello from './components/home/Fiorello';
+
+function App() {
+  return (
+    <div className="App">
+
+      <HeaderFR />
+
+      <Fiorello />
+
+      <FooterFR />
+      
+    </div>
+  );
+}
+
+
+
+export default App;
+
+
+
+
